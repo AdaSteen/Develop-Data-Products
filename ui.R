6b654fb0c7b9a4f4a5e8e106138e899c007699a6
Ada Steenekamp <steenekamp49@gmail.com>
2017-02-17 17:33:50 +0200

    Add files via upload

--- ui.R
+++ ui.R
@@ -1,37 +1,40 @@
 #
 # This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' in RStudio.
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
 #
 
 library(shiny)
 
-# Define UI for application that plot a graph showing the Central Limit Theory 
-# interactively 
+# Define UI for application that receives input from the user
 
 shinyUI(fluidPage(
     
-    # Application title
-    titlePanel("Central Limit Theorem"),
-    
-    # Sidebar with a slider input for number of coniflips and simulations
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("number",
-                        "How many coinflips:",
-                        min = 10,
-                        max = 100,
-                        value = 20,step = 10
-                        
-                        ),
-            sliderInput("nosim",
-                        "How many simulations:",
-                        min = 0,
-                        max = 1000, value = 200, step = 50)
-        ),
-        # Show the plot
-        mainPanel(
-            plotOutput("numberPlot")
-        )
-    )
+  titlePanel("The cost of diamonds -  Select applicable values for the diamond of your dreams"),
+  h3("Please select a single value for each variable"),
+  p(em("Note: if no output table is shown, please select different values - try carat(0.60), cut(Very Good), color(E), clarity(VS1)")),
+ 
+  # Front-end to receive input 
+  
+  sidebarLayout(
+      sidebarPanel(
+          
+       sliderInput("carat","CARAT (bt. 0.2 and 5.02)",0.2, 5.02, 0.6,0.01), #sliderinput for carat
+       checkboxGroupInput("cut","CUT",c("Fair","Good","Very Good","Premium","Ideal")),
+       checkboxGroupInput("color","COLOR (D - best; J - Worst",c("D","E","F","G","H","I","J")),
+       checkboxGroupInput("clarity","CLARITY (IF - Best; I1 - Worst)",c("IF","VVS2","VVS1","VS2","VS1","SI2","SI1","I1")),
+       actionButton("submit", "Submit Request") 
+       ),
+  
+#
+# Render a table with the price for all filtered records in the diamonds dataset
+#    
+
+mainPanel(
+tableOutput("diamondssubset")
+))
 ))
 
